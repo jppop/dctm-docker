91bfa3e49909963f9a618291f49643ceb51859a2
Jean-Pierre Franconieri <jppopter@gmail.com>
2015-02-09 12:40:18 +0100

    dctm-bam added (not tested)

---
+++ dctm-bam/bundles/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+dockerUsage() {
+    cat 2>&1 <<EOF
+This container must be linked with a broker (as 'broker') server.
+Something like:
+  docker run -dP --name dctm-da -h dctm-da --link broker:broker dctm-da
+EOF
+  exit 2
+}
+
+# check container links
+[ -z "${BROKER_NAME}" ] && dockerUsage
+
+CATALINA_OPTS="${CUSTOM_CATALINA_OPTS} ${CATALINA_OPTS}"
+JAVA_OPTS="${CUSTOM_JAVA_OPTS} ${JAVA_OPTS}"
+
+export CATALINA_OPTS JAVA_OPTS
+
+# configure dfc
+DFC_DATADIR=${CATALINA_HOME}/temp/dfc
+[ -d ${DFC_DATADIR} ] || mkdir -p ${DFC_DATADIR}
+
+cat << __EOF__
+dcf.name=bam
+dfc.data.dir=${DFC_DATADIR}
+dfc.tokenstorage.enable=false
+dfc.docbroker.host[0]=${DOCBROKER_ADR:-$BROKER_PORT_1489_TCP_ADDR}
+dfc.docbroker.port[0]=${DOCBROKER_PORT:-$BROKER_PORT_1489_TCP_PORT}
+dfc.session.secure_connect_default=try_native_first
+dfc.globalregistry.repository=${REGISTRY_NAME:-devbox}
+dfc.globalregistry.username=${REGISTRY_USER:-dm_bof_registry}
+dfc.globalregistry.password=${REGISTRY_CRYPTPWD:-AAAAEGksM99HhP8PaQO7r43ADePXDPKXd+lEei1ddxmWgnBv}
+dfc.session.allow_trusted_login = false
+__EOF__
+
+echo "xcp.repository.name=${REPOSITORY_NAME}" > conf/deployment.properties
+
+cat << __EOF__ >> conf/bam.properties
+bam.jdbc.userName=${BAM_USER:-bamdbo}
+bam.jdbc.password=${BAM_PWD:-bamdbo}
+bam.dfc.session.repository=${REPOSITORY_NAME}
+bam.dfc.session.repositoryUserName=${REPOSITORY_USER}
+bam.dfc.session.repositoryPassword=${REPOSITORY_PWD}
+__EOF__
+
+echo "DFC Config file:"
+cat conf/dfc.properties
+
+echo "Using CATALINA_OPTS:   ${CATALINA_OPTS}"
+echo "Using JAVA_OPTS:       ${JAVA_OPTS}"
+exec "$@"
