a14657361d773ddd16ec579c137e1922c30420e3
admin <admin@localhost.localdomain>
2015-02-16 13:05:17 -0100

    improve build script

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-LOG_DIR=~/Library/Logs
+LOG_DIR=./logs
 LOG_FILE=${LOG_DIR}/docker-build.log
 
+[ -d "$LOG_DIR" ] || mkdir -p $LOG_DIR
 touch ${LOG_FILE}
 
 for img in $(cat images.lst); do
+  if [[ "$img" == "#"* ]]; then
+    echo "${img#\#} skipped"
+  else
 	echo "Building $img..."
 	[ -w ${LOG_FILE} ] && logger -s "Starting build. Image: $img..." 2>> ${LOG_FILE}
 	docker build -t $img $img/ 2>&1 | tee ${LOG_DIR}/docker-build-$img.log
 	[ -w ${LOG_FILE} ] && logger -s "Done. Image: $img..." 2>> ${LOG_FILE}
+  fi
 done
