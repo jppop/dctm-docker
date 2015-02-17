1341d026fa4cd58d59d028ff1be17d78a03664b6
Jean-Pierre Franconieri <jppopter@gmail.com>
2015-02-17 16:46:54 +0100

    update readme

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+repo=${1:-devbox}
+export REPOSITORY_NAME $repo
+
 docker run -dP --name dbora -h dbora oracle-xe
 #docker run -dP --name broker -h broker dctm-broker
-docker run -dP --name dctm-cs -h dctm-cs --link dbora:dbora dctm-cs
+docker run -dP -p 1489:1489 -p 49000:49000 --name dctm-cs -h dctm-cs --link dbora:dbora dctm-cs --repository-name $repo
