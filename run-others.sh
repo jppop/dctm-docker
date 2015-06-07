2d13974791e33582b2b4808b8df5427545c053a2
jfrancon <jean-pierre.franconieri@soprasteria.com>
2015-06-07 19:00:28 +0200

    run-others-less no more usefull

--- run-others.sh
+++ run-others.sh
@@ -51,18 +51,13 @@ done
 [ -z "$HOST_IP" ]  && die "No host ip." 1
 [ -z "$containers" ]  && die "No containers." 1
 
-dctm_cs=$(docker inspect --format '{{ .Config.Env }}' dctm-cs | grep "REPOSITORY_NAME=$repo")
-[ -z "$dctm_cs" ] && die "Container dctm-cs (with repo $repo) not found." 2
-# try to check if dctm-xs finished the installation
-marker=$(docker exec -it dctm-cs ls -a1 ${DM_HOME}/install/.stop-install)
-[ -z "$marker" ]  && die "Seems dctm-cs installation not finished yet. Check the logs: docker logs -f dctm-cs" 3
-
 DOCUMENTUM=/opt/documentum
 DOCUMENTUM_SHARED=${DOCUMENTUM}/shared
 DM_HOME=${DOCUMENTUM}/product/7.1
 
-# try to check if dctm-cs finished the installation
-#marker=$(docker exec -it dctm-cs ls -a1 /opt/documentum/product/7.1/install/.stop-install)
+dctm_cs=$(docker inspect --format '{{ .Config.Env }}' dctm-cs | grep "REPOSITORY_NAME=$repo")
+[ -z "$dctm_cs" ] && die "Container dctm-cs (with repo $repo) not found." 2
+# try to check if dctm-xs finished the installation
 marker=$(docker exec -it dctm-cs ls -a1 ${DM_HOME}/install/.stop-install)
 [ -z "$marker" ]  && die "Seems dctm-cs installation not finished yet. Check the logs: docker logs -f dctm-cs" 3
 
