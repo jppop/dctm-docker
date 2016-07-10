648c696710ab9b6c976c514d2c48e4eb12d4abf6
Jean-Pierre FRANCONIERI <jppopter@gmail.com>
2016-07-10 05:33:58 -0100

    xcp 2.2

--- dctm-catalog/install.sh
+++ dctm-catalog/install.sh
@@ -5,20 +5,20 @@ SOURCE=~/distrib
 TARGET=..
 
 echo "Copying BAM bundle"
-cp $SOURCE/documentum/bam/2.1/bam-server.war $TARGET/dctm-bam/bundles/
+cp $SOURCE/documentum/bam/2.2/bam-server.war $TARGET/dctm-bam/bundles/
 
 echo "Copying BPS bundle"
-cp $SOURCE/documentum/bps/2.1/bps.war $TARGET/dctm-bps/bundles/
+cp $SOURCE/documentum/bps/2.2/bps.war $TARGET/dctm-bps/bundles/
 
 echo "Copying Content Server bundle"
 cp $SOURCE/documentum/content-server/7.1/Content_Server_7.1_linux64_oracle.tar $TARGET/dctm-base/bundles/
 [ -d $TARGET/dctm-base/bundles/patch ] || mkdir -p $TARGET/dctm-base/bundles/patch
 #cp $SOURCE/documentum/content-server/7.1/CS_7.1.0090/*.* $TARGET/dctm-base/bundles/patch/
-cp $SOURCE/documentum/content-server/7.1/CS_7.1.0250/*.* $TARGET/dctm-base/bundles/patch/
+cp $SOURCE/documentum/content-server/7.1/CS_7.1.0260/*.* $TARGET/dctm-base/bundles/patch/
 
 echo "Copying Repository bundle"
 cp -r $SOURCE/documentum/content-server/7.1/dars $TARGET/dctm-cs/bundles/dars
-cp $SOURCE/documentum/process-engine/2.1/Process_Engine_linux.tar $TARGET/dctm-cs/bundles/
+cp $SOURCE/documentum/process-engine/2.2/Process_Engine_linux.tar $TARGET/dctm-cs/bundles/
 cp $SOURCE/oracle/11.2/*.* $TARGET/dctm-cs/bundles/
 
 echo "Copying DA bundle"
@@ -40,6 +40,7 @@ cp $SOURCE/documentum/xms-tools/1.2/xms-tools-1.2.zip $TARGET/dctm-xmstools/bund
 echo "Copying xPlore bundle"
 cp $SOURCE/documentum/xplore/1.4/xPlore_1.4_linux-x64.tar $TARGET/dctm-xplore/bundles/
 cp $SOURCE/oracle/java/1.7/jdk-7u79-linux-x64.rpm $TARGET/dctm-xplore/bundles/
+cp $SOURCE/apache-ant/1.9/*.* $TARGET/dctm-xplore/bundles/
 
 if [  -d $TARGET/dctm-xpression ]; then
 	echo "Copying xPression bundle"
